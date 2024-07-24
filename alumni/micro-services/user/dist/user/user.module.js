7ad593cc9256b7d34e4d2648c68107ab5b9241f5
Niel <sympta113@gmail.com>
2024-07-24 16:04:42 +0200

    comunication user avec la BDD

--- alumni/micro-services/user/dist/user/user.module.js
+++ alumni/micro-services/user/dist/user/user.module.js
@@ -12,13 +12,14 @@ const user_service_1 = require("./user.service");
 const user_controller_1 = require("./user.controller");
 const user_entity_1 = require("./entities/user.entity");
 const typeorm_1 = require("@nestjs/typeorm");
+const user_repository_1 = require("./user.repository");
 let UserModule = class UserModule {
 };
 exports.UserModule = UserModule;
 exports.UserModule = UserModule = __decorate([
     (0, common_1.Module)({
         controllers: [user_controller_1.UserController],
-        providers: [user_service_1.UserService],
+        providers: [user_service_1.UserService, user_repository_1.UserRepository],
         imports: [typeorm_1.TypeOrmModule.forFeature([user_entity_1.UserEntity])],
     })
 ], UserModule);
