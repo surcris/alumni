3f9e826e5fb5f3c86eea423ee5db1704483ae999
nathan <nathan.stievano@poe.aelion.fr>
2024-07-25 10:52:20 +0200

    add logic to front for US6 :sparkles:

--- alumni/alumni-mob/src/app/first-connexion/components/change-password/change-password.component.ts
+++ alumni/alumni-mob/src/app/first-connexion/components/change-password/change-password.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ChangePasswordService } from '../../services/change-password.service';
 import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { StorageService } from 'src/app/core/services/storage.service';
+import { take } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-change-password',
@@ -18,6 +21,7 @@ export class ChangePasswordComponent  implements OnInit {
     private _service: ChangePasswordService,
     private _toastController: ToastController,
     private _router: Router,
+    private _storage: StorageService
   ) { }
 
   ngOnInit(): void {
@@ -38,7 +42,43 @@ export class ChangePasswordComponent  implements OnInit {
   }
 
   onSubmit() {
-    throw new Error('Method not implemented.');
+    if (this.form.value.password === this.form.value.confirmPassword){
+      const email = this._storage.retrieve('email')
+      if (email===null){
+        this.createToast('Une erreur est survenue veuillez réessayer tout le processus','')
+      }
+      this._service.changePassword(email,this.form.value.password)
+      .pipe(
+        take(1)
+      )
+      .subscribe({
+        next: async(response: HttpResponse<any>) => {
+          if (response.status === 200) {
+            this.createToast('Votre compte a bien été créé', 'Ok')
+            this._router.navigate(['login'])
+          } else {
+            this.createToast(`Une erreur est survenue lors de l'initilaisation de votre mot de passe veuillez réessayer`, 'Réessayer')
+          }
+        }
+      })
+    }else{
+      this.createToast('Les deux mot de passe ne sont pas identique', 'Réessayer')
     }
-    
+  }
+
+  async createToast(message: string, textButton: string) {
+    const toast = await this._toastController.create({
+      message: message,
+      duration: 5000,
+      position: 'top',
+      buttons: [
+        {
+          text: textButton,
+        }
+      ]
+    })
+    await toast.present()
+    toast.onWillDismiss()
+      .then(() => this.form.reset())
+  }    
 }
--- alumni/alumni-mob/src/app/first-connexion/services/change-password.service.ts
+++ alumni/alumni-mob/src/app/first-connexion/services/change-password.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChangePasswordService {
 
-  constructor() { }
+  private readonly URI: string = 'http://localhost:3000/user'
+
+  constructor(
+    private _httpClient: HttpClient
+  ) { }
+
+  /**
+   * Get a statut that show if the password has changed correctly or not 
+   * @returns Observable<HttpResponse<any>>
+   */
+    public changePassword(email: string, password: string): Observable<HttpResponse<any>>{
+      const payload = { 
+        email: email,
+        password: password
+      };
+      return this._httpClient.patch<HttpResponse<any>>(
+        this.URI + '/' + email,
+        payload
+      )
+    }
+
 }
