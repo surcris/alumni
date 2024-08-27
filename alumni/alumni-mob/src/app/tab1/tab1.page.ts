7e080a77c09b76be2e5d84f1434c7b08846ecea3
Niel <sympta113@gmail.com>
2024-08-27 16:25:36 +0200

    bouton deconnexion

--- alumni/alumni-mob/src/app/tab1/tab1.page.ts
+++ alumni/alumni-mob/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,42 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ]
+  constructor(
+    private _alertController: AlertController,
+    private _route: Router
+  ) {}
 
+  async logout(){
+    const alertLogout = await this._alertController.create({
+      header: 'Déconnexion',
+      // subHeader: 'A Sub Header Is Optional',
+      message: 'Etes-vous sur de vouloir vous déconnectez.',
+      buttons: ['Cancel',{
+        text: 'OK',
+        role: 'confirm',
+        handler: () => {
+          localStorage.removeItem('auth')
+          this._route.navigate(['login'])
+          // this._menuController.close()
+        },
+      }],
+    })
+    await alertLogout.present();
+  }
 }
